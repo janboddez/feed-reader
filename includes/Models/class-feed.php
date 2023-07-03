582789664c4606db714b481746ec0d1faa82fb98
Jan Boddez <jan.boddez@gmail.com>
2023-07-03 22:34:43 +0200

    Better ordering (but requires MySQL 8+?)

--- includes/Models/class-feed.php
+++ includes/Models/class-feed.php
@@ -41,7 +41,7 @@ class Feed extends Model {
 			$search = str_replace( array( '\\', '_', '%' ), array( '\\\\', '\\_', '\\%' ), $search );
 
 			$sql .= $wpdb->prepare(
-				' WHERE user_id = %d AND (url LIKE %s OR name LIKE %s) ORDER BY url ASC LIMIT %d OFFSET %d',
+				' WHERE user_id = %d AND (url LIKE %s OR name LIKE %s) ORDER BY ISNULL(NULLIF(last_error, "")), REGEXP_REPLACE(url, "^https?://", "") ASC LIMIT %d OFFSET %d',
 				get_current_user_id(),
 				"%$search%",
 				"%$search%",
@@ -49,7 +49,7 @@ class Feed extends Model {
 				$offset
 			);
 		} else {
-			$sql .= $wpdb->prepare( ' WHERE user_id = %d ORDER BY url ASC LIMIT %d OFFSET %d', get_current_user_id(), $limit, $offset );
+			$sql .= $wpdb->prepare( ' WHERE user_id = %d ORDER BY ISNULL(NULLIF(last_error, "")), REGEXP_REPLACE(url, "^https?://", "") ASC LIMIT %d OFFSET %d', get_current_user_id(), $limit, $offset );
 		}
 
 		$total = preg_replace( '~^SELECT \*~', 'SELECT COUNT(*)', $sql );
@@ -58,8 +58,7 @@ class Feed extends Model {
 		$sql = sprintf(
 			'SELECT f.*, c.name AS category_name
 			 FROM (%s) AS f
-			 LEFT JOIN %s AS c ON c.id = f.category_id
-			 ORDER BY f.url',
+			 LEFT JOIN %s AS c ON c.id = f.category_id',
 			$sql,
 			Category::table()
 		);
