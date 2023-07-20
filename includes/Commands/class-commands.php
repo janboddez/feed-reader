bc9df8eba77df11b23b8d14c20169594c8fff655
Jan Boddez <jan.boddez@gmail.com>
2023-07-21 01:37:30 +0200

    Add --days argument

--- includes/Commands/class-commands.php
+++ includes/Commands/class-commands.php
@@ -7,10 +7,24 @@ use WP_CLI;
 
 class Commands {
 	/**
-	 * Permanently deletes stale entries over 30 days old.
+	 * Permanently deletes old "stale" entries.
+	 *
+	 * [--days=<days>]
+	 * : Delete only entries older than this number of days.
+	 *
+	 * @param array $args       (Optional) arguments.
+	 * @param array $assoc_args (Optional) "associated" arguments.
 	 */
-	public function cleanup() {
-		$max_timestamp = date( 'Y-m-d H:i:s', time() - 30 * DAY_IN_SECONDS );
+	public function cleanup( $args, $assoc_args ) {
+		$days = 30;
+
+		if ( isset( $assoc_args['days'] ) && ctype_digit( (string) $assoc_args['days'] ) ) {
+			$days = (int) $assoc_args['days'];
+		}
+
+		$max_timestamp = date( 'Y-m-d H:i:s', time() - $days * DAY_IN_SECONDS );
+
+		WP_CLI::line( "Looking at entries older than {$max_timestamp} UTC." );
 
 		global $wpdb;
 
@@ -25,8 +39,8 @@ class Commands {
 
 		WP_CLI::line( "Found {$count} entries to be removed." );
 
-		// $sql = sprintf( 'DELETE FROM %s WHERE published < %%s AND in_feed = %%d AND (is_read = %%d OR deleted_at IS NOT NULL)', Entry::table() );
-		// $wpdb->query( $wpdb->prepare( $sql, $max_timestamp, 0, 1 ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.PreparedSQL.NotPrepared
+		$sql = sprintf( 'DELETE FROM %s WHERE published < %%s AND in_feed = %%d AND (is_read = %%d OR deleted_at IS NOT NULL)', Entry::table() );
+		$wpdb->query( $wpdb->prepare( $sql, $max_timestamp, 0, 1 ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.PreparedSQL.NotPrepared
 
 		WP_CLI::success( 'All done!' );
 	}
