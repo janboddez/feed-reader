b4618358430eff2ade1685df2ea6f6ad45b240da
Jan Boddez <jan.boddez@gmail.com>
2023-07-22 00:28:44 +0200

    Add subscribe command, just because

--- includes/Commands/class-commands.php
+++ includes/Commands/class-commands.php
@@ -2,10 +2,66 @@
 
 namespace FeedReader\Commands;
 
+use FeedReader\Jobs\Poll_Feeds;
+use FeedReader\Models\Category;
 use FeedReader\Models\Entry;
+use FeedReader\Models\Feed;
 use WP_CLI;
 
 class Commands {
+	/**
+	 * Subscribes to a feed.
+	 *
+	 * <url>
+	 * : The URL of the feed.
+	 *
+	 * [--cat=<cat>]
+	 * : The ID of the category to use. Must belong to the "current" user.
+	 *
+	 * @param array $args       Arguments.
+	 * @param array $assoc_args "Associated" arguments.
+	 */
+	public function subscribe( $args, $assoc_args ) {
+		$url = trim( $args[0] );
+
+		if ( ! preg_match( '~^https?://~', $url ) ) {
+			$url = 'http://' . ltrim( $url, '/' );
+		}
+
+		if ( ! empty( $assoc_args['cat'] ) && ctype_digit( (string) $assoc_args['cat'] ) ) {
+			// Look up category.
+			$category = Category::find( (int) $assoc_args['cat'] );
+		}
+
+		$feed_id = Feed::exists( esc_url_raw( $url ) );
+
+		if ( ! $feed_id ) {
+			$feed_id = Feed::insert(
+				array(
+					'url'         => esc_url_raw( $url ),
+					'name'        => preg_replace( '~^www~', '', wp_parse_url( $url, PHP_URL_HOST ) ),
+					'category_id' => isset( $category ) ? $category->id : null,
+					'user_id'     => get_current_user_id(),
+				)
+			);
+		} else {
+			WP_CLI::line( 'The user with ID ' . get_current_user_id() . ' is already subscribed to the feed at ' . esc_url_raw( $url ) . '.' );
+		}
+
+		if ( $feed_id ) {
+			$feed = Feed::find( $feed_id );
+
+			if ( false === get_transient( "feed-reader:feeds:{$feed->id}:avatar" ) ) {
+				Feed::fetch_favicon( $feed );
+			}
+
+			WP_CLI::line( 'Polling the feed at ' . esc_url_raw( $url ) . '.' );
+			Poll_Feeds::poll_feed( $feed );
+
+			WP_CLI::success( 'All done!' );
+		}
+	}
+
 	/**
 	 * Permanently deletes old "stale" entries.
 	 *
