0aac0741b93eff38bbadc94c226e3d557efa0bf8
Jan Boddez <jan.boddez@gmail.com>
2023-10-25 16:50:49 +0200

    Add mf2 props

--- includes/views/entries/view.php
+++ includes/views/entries/view.php
@@ -20,23 +20,64 @@ $user_settings = get_user_meta( get_current_user_id(), 'feed_reader_settings', t
 		<?php else : ?>
 			<article class="hentry <?php echo esc_attr( ! empty( $entry->name ) ? 'article' : 'note' ); ?>" data-id="<?php echo esc_attr( $entry->id ); ?>" data-feed-id="<?php echo esc_attr( $entry->feed_id ); ?>">
 		<?php endif; ?>
-			<?php if ( ! empty( $entry->name ) ) : ?>
-				<h1 class="entry-title"><?php echo esc_html( $entry->name ); ?></h1>
-			<?php elseif ( ! empty( $entry->summary ) ) : ?>
-				<h1 class="screen-reader-text"><?php echo esc_html( $entry->summary ); ?></h1>
-			<?php endif; ?>
 
-			<?php static::render( 'entries/partials/entry-meta', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope ?>
+			<?php
+			if ( ! empty( $entry->data ) ) :
+				$data = json_decode( $entry->data, true );
 
-			<?php if ( ! empty( $entry->content ) ) : ?>
-				<div class="entry-content">
-					<?php echo \FeedReader\Helpers\proxy_images( $entry->content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-				</div>
-				<?php
+				if ( ! empty( $data['properties']['in-reply-to'][0]['value'] ) ) :
+					?>
+					<a href="<?php echo esc_url( $data['properties']['in-reply-to'][0]['value'] ); ?>" class="reply"><?php echo esc_url( $data['properties']['in-reply-to'][0]['value'] ); ?></a>
+					<?php
+				elseif ( ! empty( $data['properties']['bookmark-of'][0]['value'] ) ) :
+					?>
+					<a href="<?php echo esc_url( $data['properties']['bookmark-of'][0]['value'] ); ?>" class="bookmark"><?php echo esc_url( $data['properties']['bookmark-of'][0]['value'] ); ?></a>
+					<?php
+				endif;
 			endif;
 
-			static::render( 'entries/partials/entry-actions', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope
+			if ( ! empty( $entry->name ) ) :
+				?>
+				<h1 class="entry-title"><?php echo esc_html( $entry->name ); ?></h1>
+
+				<?php static::render( 'entries/partials/entry-meta', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope ?>
+
+				<?php if ( ! empty( $entry->content ) ) : ?>
+					<div class="entry-content">
+						<?php echo \FeedReader\Helpers\proxy_images( $entry->content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+					<?php
+				endif;
+
+				static::render( 'entries/partials/entry-actions', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope
+			elseif ( ! empty( $entry->summary ) ) :
+				?>
+				<h1 class="screen-reader-text"><?php echo esc_html( $entry->summary ); ?></h1>
+
+				<?php if ( ! empty( $entry->content ) ) : ?>
+					<div class="entry-content">
+						<?php echo \FeedReader\Helpers\proxy_images( $entry->content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+					<?php
+				endif;
+
+				static::render( 'entries/partials/entry-meta', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope
+
+				static::render( 'entries/partials/entry-actions', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope
+			else :
+				?>
+				<?php if ( ! empty( $entry->content ) ) : ?>
+					<div class="entry-content">
+						<?php echo \FeedReader\Helpers\proxy_images( $entry->content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+					<?php
+				endif;
+
+				static::render( 'entries/partials/entry-meta', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope
+
+				static::render( 'entries/partials/entry-actions', compact( 'entry' ) ); // phpcs:ignore PHPCompatibility.Classes.NewLateStaticBinding.OutsideClassScope
+			endif;
 			?>
-		</article>
+	</article>
 	</div>
 </div>
